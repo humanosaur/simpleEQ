682b6aea0eb2cdf8ffb5f8d9ec471f73be3ecc44
humanosaur <jskonie@gmail.com>
2024-02-05 14:55:14 -0600

    Added gain labels to grid

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -406,6 +406,26 @@ void ResponseCurveComponent::resized()
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
     
+    for( auto gDb : gain )
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+        
+        String str;
+        if( gDb > 0 )
+            str << "+";
+        str << gDb;
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r. setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+        
+        g.setColour(gDb == 0.f ? Colours::white : Colour::fromFloatRGBA(255.f,255.f,255.f,0.6f));
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
